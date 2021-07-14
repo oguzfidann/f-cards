e4fd0848582ccedfacae4802ceb9a70eb16002ad
Oğuz Fidan <85435535+oguzfidann@users.noreply.github.com>
2021-07-14 16:13:36 +0300

    Add files via upload

---
+++ footballer cards/js/script.js
@@ -0,0 +1,13 @@
+/*
+
+let names = document.querySelector(".name");
+
+let info = document.querySelector("#info");
+
+for (i = 0; i < names; i++) {
+  let div = document.createElement("div");
+  div.innerHTML = names[i];
+  info.appendChild(div);
+}
+
+*/
